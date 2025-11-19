448e88e2250cfeb6c378725f23742b85e3b7b042
Tomáš Gordoň <tomasgordon.sk@gmail.com>
2025-11-19 13:16:46 +0100

    feat(context): :building_construction: encapsulate content with dashboard context provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import React from "react";
+
 import Header from "./layout/Header";
 import Dashboard from "./layout/Dashboard";
+import { DashboardProvider } from "./core/context/DashboardContext";
 
 const App: React.FC = () => {
   return (
-    <div>
-      <Header />
-      <Dashboard />
-    </div>
+    <DashboardProvider>
+      <div className="min-h-screen text-gray-200 font-sans">
+        <Header />
+        <main className="p-4 sm:p-6 lg:p-8">
+          <Dashboard />
+        </main>
+      </div>
+    </DashboardProvider>
   );
 };
 
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -5,10 +5,14 @@ import {
   PlusIcon,
 } from "@heroicons/react/24/outline";
 
+import { useDashboard } from "../core/context/DashboardContext";
+
 const Header: React.FC = () => {
+  const { dispatch } = useDashboard();
+
   return (
     <header className="bg-gray-800 shadow-md p-4 flex justify-between items-center">
-      <h1 className="text-xl text-cyan-400">Dashboard</h1>
+      <h1 className="text-xl text-yellow-400">Dashboard</h1>
 
       <div className="flex items-center space-x-2 sm:space-x-5">
         <button
@@ -25,9 +29,12 @@ const Header: React.FC = () => {
           <ArrowUpTrayIcon className="size-5" />
           <span className="hidden sm:inline">Export</span>
         </button>
-        <button className="flex items-center space-x-2 bg-cyan-500 hover:bg-cyan-600 text-white font-extralight py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 shadow-lg">
+        <button
+          onClick={() => dispatch({ type: "OPEN_ADD_MODAL" })}
+          className="flex items-center space-x-2 bg-yellow-400 hover:bg-yellow-500 text-white font-extralight py-2 px-4 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 shadow-lg"
+        >
           <PlusIcon className="size-5" />
-          <span className="hidden sm:inline">Widgets</span>
+          <span className="hidden sm:inline">Widget</span>
         </button>
       </div>
     </header>
